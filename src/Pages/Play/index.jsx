60ae9fbee72b8f531330d46491d8b3139626b149
chain <klaus.mika1052@gmail.com>
2023-04-21 16:45:17 -0300

    adicionado o pillar esquerdo na página de jogar

--- src/Pages/Play/index.jsx
+++ src/Pages/Play/index.jsx
@@ -1,11 +1,22 @@
 import styles from "./styles.module.css";
+import leftPillar from "../../assets/left-pillar.svg";
+import rightPillar from "../../assets/right-pillar.svg";
+import spiderWebLeft from "../../assets/spider-web1.svg";
+import spiderWebRight from "../../assets/spider-web2.svg";
 
 const Play = () => {
   return (
     <div className="bg">
       <div className="containerApp">
-        <div className={styles.app}></div>
+        <div className={styles.app}>
+          <img
+            className={styles.leftPillar}
+            src={leftPillar}
+            alt="pillar direito"
+          />
+        </div>
       </div>
+      /
     </div>
   );
 };
