1160bf91198487137f39fd0ba7a9a1901b4fdd5e
chain <klaus.mika1052@gmail.com>
2023-04-22 15:27:59 -0300

    adicionado de forma aleatória diamante ou bomba nos cards

--- src/Components/CardBoard/CardItem/index.jsx
+++ src/Components/CardBoard/CardItem/index.jsx
@@ -1,33 +1,90 @@
 import React from "react";
 import styles from "./styles.module.css";
+import PropTypes from "prop-types";
 import cardItemFrontImage1 from "../../../assets/table-item1.svg";
 import cardItemFrontImage2 from "../../../assets/table-item2.svg";
 import cardItemFrontImage3 from "../../../assets/table-item3.svg";
 import cardItemFrontImage4 from "../../../assets/table-item4.svg";
+import cardItemBackImage1 from "../../../assets/table-item-back1.svg";
+import cardItemBackImage2 from "../../../assets/table-item-back2.svg";
+import cardItemBackImage3 from "../../../assets/table-item-back3.svg";
+import cardItemBackImage4 from "../../../assets/table-item-back4.svg";
+import diamondPurpleImage from "../../../assets/diamond-purple.svg";
+import diamondRedImage from "../../../assets/diamond-red.svg";
+import diamondYellowImage from "../../../assets/diamond-yellow.svg";
+import bombImage from "../../../assets/bomb.svg";
 
 const cardItemArray = [
-  cardItemFrontImage1,
-  cardItemFrontImage2,
-  cardItemFrontImage3,
-  cardItemFrontImage4,
+  {
+    front: cardItemFrontImage1,
+    back: cardItemBackImage1,
+  },
+  {
+    front: cardItemFrontImage2,
+    back: cardItemBackImage2,
+  },
+  {
+    front: cardItemFrontImage3,
+    back: cardItemBackImage3,
+  },
+  {
+    front: cardItemFrontImage4,
+    back: cardItemBackImage4,
+  },
+];
+const diamondItemArray = [
+  diamondPurpleImage,
+  diamondRedImage,
+  diamondYellowImage,
 ];
 
-const CardItem = () => {
-  const [card, setCard] = React.useState(null);
+const CardItem = (props) => {
+  const [card, setCard] = React.useState({});
+  const [open, setOpen] = React.useState(false);
+  const { itemCard, diamond, bomb } = props.cardData;
 
   React.useEffect(() => {
     function takeRadomCard() {
-      setCard(cardItemArray[Math.floor(Math.random() * 3)]);
-      console.log(card);
+      setCard({
+        front: cardItemArray[itemCard].front,
+        back: cardItemArray[itemCard].back,
+        diamond: diamondItemArray[diamond],
+        bomb: bomb ? bombImage : false,
+      });
     }
     takeRadomCard();
-  }, [card]);
+  }, [itemCard, diamond, bomb]);
+
+  function handleClick() {
+    setOpen(true);
+  }
+  console.log(card);
 
   return (
-    <button className={styles.cardItem}>
-      <img src={card} alt="imagem do card" />
-    </button>
+    <div onClick={handleClick} className={styles.cardItem}>
+      {open ? (
+        <button
+          className={styles.cardOpened}
+          style={{ background: `url(${card.back})no-repeat` }}
+        >
+          <img
+            className={styles.cardOpenedResult}
+            src={card.bomb ? card.bomb : card.diamond}
+            alt={card.bomb ? "imagem de bomba" : "imagem de diamante"}
+          />
+        </button>
+      ) : (
+        <button
+          className={styles.cardClosed}
+          style={{ background: `url(${card.front})no-repeat` }}
+        ></button>
+      )}
+    </div>
   );
+};
+
+CardItem.propTypes = {
+  cardData: PropTypes.object,
 };
 
 export default CardItem;
--- src/Components/CardBoard/index.jsx
+++ src/Components/CardBoard/index.jsx
@@ -3,11 +3,27 @@ import styles from "./styles.module.css";
 
 const CardBoard = () => {
   const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const randomCards = takeRandomCard(cards);
 
+  function takeRandomNumber(InitialNumber, finalNumber) {
+    return Math.floor(Math.random() * finalNumber) + InitialNumber;
+  }
+
+  function takeRandomCard(cards) {
+    const takeRandomBombPosition = takeRandomNumber(1, 10);
+    return cards.map((card) => {
+      const takeRandomCardItem = takeRandomNumber(0, 4);
+      return {
+        itemCard: takeRandomCardItem,
+        diamond: takeRandomNumber(0, 3),
+        bomb: card === takeRandomBombPosition,
+      };
+    });
+  }
   return (
     <div className={styles.cardBoard}>
-      {cards.map((card) => (
-        <CardItem key={card} />
+      {randomCards.map((card, index) => (
+        <CardItem key={index} cardData={card} />
       ))}
     </div>
   );
