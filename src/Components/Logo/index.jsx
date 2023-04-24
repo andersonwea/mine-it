72599359cacdef4ddd5a206b5395e943c29a4eb5
chain <klaus.mika1052@gmail.com>
2023-04-24 16:43:39 -0300

    add dist

--- src/Components/Logo/index.jsx
+++
@@ -1,8 +0,0 @@
-import logoImage from "../../assets/logo.svg";
-import styles from "./styles.module.css";
-
-const Logo = () => {
-  return <img className={styles.logo} src={logoImage} alt="logo do mine it" />;
-};
-
-export default Logo;
